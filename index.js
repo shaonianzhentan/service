ae00de52c740a2edfb0ebd0d310690626d4a7603
shaonianzhentan <shaonianzhentan@msn.cn>
2017-09-05 20:27:53 +0800

    更新

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var express = require('express');
+var express = require('express')
+    , cors = require('cors');
 var app = express();
 
+app.use(cors());
 
 app.get('/', function (req, res) {
     res.send('hello world');
